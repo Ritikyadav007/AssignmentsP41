8eb95aeaaf57717f3156063a8f74e0926a676fae
Ritikyadav007 <ritiky986@gmail.com>
2022-05-31 15:12:16 +0530

    First commit

--- Assignment1/profile_list/src/App.js
+++ Assignment1/profile_list/src/App.js
@@ -1,11 +1,38 @@
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     Hello
-    </div>
-  );
+
+class App extends Component() {
+  state = {data: [], isLoaded: false}
+
+
+  
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(json =>{
+      this.setState({data: json, isLoaded: true})
+      
+    });
+    
+  }
+
+  render(){
+    var {data, isLoaded} = this.state;
+    if(!isLoaded){
+      return <div>Loding..</div>
+    }
+    else{
+      return (
+        <div className="App">
+          {data.map(item =>(
+          <h1>{item.username}</h1>
+        ))}
+        </div>
+      );
+    }  
+  }
 }
 
 export default App;
