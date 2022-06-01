1f4604968fdb963813e856d79e0daec719ec40fc
Ritikyadav007 <ritiky986@gmail.com>
2022-06-01 13:35:17 +0530

    Fetched data from API

--- Assignment1/list_profile/src/App.tsx
+++ Assignment1/list_profile/src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type MyProps = {};
+
+// type User=  {
+//     id,	// The user's id
+//     username,
+//     name,
+//     email,
+//     phone,
+//     website,
+//     address: {
+// 	  street, // Address line 1
+// 	  suite, // Address line 2
+// 	  city,
+// 	  zipcode
+//     },
+//     company: {
+// 	  name, // The name of company where the user works
+//     }
+//   }
+
+type MyState = {
+  data: { id: number }[];
+  isLoaded: boolean; // like this
+};
+export default class App extends Component<MyProps, MyState> {
+  state = { data: [], isLoaded: false };
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((json) => {
+        this.setState({ data: json, isLoaded: true });
+      });
+  }
+  render() {
+    var { data, isLoaded } = this.state;
+    if (!isLoaded) {
+      return <div>Loding..</div>;
+    } else {
+      return (
+        <div>
+          {data.length > 0 &&
+            data.map((item) => {
+              console.log(item);
+              const { id, username } = item;
+              return <h1>{id}</h1>;
+            })}
+        </div>
+      );
+    }
+  }
 }
-
-export default App;
