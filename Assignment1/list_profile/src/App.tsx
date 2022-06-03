d25f0ef91230a2f3990988784f11dcb59fb9b060
Ritikyadav007 <ritiky986@gmail.com>
2022-06-03 12:39:31 +0530

    final commit with improvements

--- Assignment1/list_profile/src/App.tsx
+++ Assignment1/list_profile/src/App.tsx
@@ -4,19 +4,37 @@ import './App.css';
 
 type MyProps = {};
 
+export type user = {
+    id : number;	// The user's id
+    username: string;
+    name: string;
+    email: string;
+    phone: string;
+    website: string;
+    address: {
+	  street: string;
+	  suite: string;
+	  city: string;
+	  zipcode: string;
+    },
+    company: {
+	  name: string;
+    }
+  }
+
+
 type MyState = {
-  data: any;
+  data: Array<user>;
   isLoaded: boolean; // like this
 };
 export default class App extends Component<MyProps, MyState> {
-  state = { data: [], isLoaded: false };
+  state = { data: [] , isLoaded: false };
 
   componentDidMount() {
+
     const getUsers =async () => {
       const response = await fetch("https://jsonplaceholder.typicode.com/users");
       const users = await response.json();
-
-
       this.setState({data: users, isLoaded:true});
     }
 
--- Assignment1/list_profile/src/Components/ImageCard.tsx
+++ Assignment1/list_profile/src/Components/ImageCard.tsx
@@ -1,44 +1,57 @@
 import React, { Component } from "react";
+import { user } from "../App";
+import Card from "./Card";
 import UserDetails from "./UserDetails";
 
 type ImageCardPropTypes = {
-  user: any;
+  user: user;
 };
 
 export default class ImageCard extends Component<ImageCardPropTypes> {
-
-
-    renderBody(){
-        const {email, phone, name, website, company, address} = this.props.user;
-        return(
-            <div className="card-body">
-            <h2 className="card-title">{name}</h2>
-            <UserDetails detailName ="Email: " value ={email}/>
-            <UserDetails detailName ="Phone: " value ={phone}/>
-            <UserDetails detailName ="Company: " value ={company.name}/>
-            <UserDetails detailName ="Website: " value ={website}/>
-            <UserDetails detailName ="Address: " value ={address.street +' '+ address.suite +' '+ address.city +' '+ address.zipcode}/>
-          </div>
-        )
-    }
+  // card details
+  renderBody() {
+    const { email, phone, name, website, company, address } = this.props.user;
+    return (
+      <div className="card-body">
+        <h2 className="card-title">{name}</h2>
+        <UserDetails detailName="Email: " value={email} />
+        <UserDetails detailName="Phone: " value={phone} />
+        <UserDetails detailName="Company: " value={company.name} />
+        <UserDetails detailName="Website: " value={website} />
+        <UserDetails
+          detailName="Address: "
+          value={
+            address.street +
+            " " +
+            address.suite +
+            " " +
+            address.city +
+            " " +
+            address.zipcode
+          }
+        />
+      </div>
+    );
+  }
 
   render() {
-    const { username} = this.props.user;
-    
-
-
+    const { username } = this.props.user;
 
     return (
-      <div className="card mb-3 " style={{maxWidth:'1110px',  maxHeight:'220px', padding:'10px', boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)'}}>
+      // card structure
+      <Card>
         <div className="row g-0">
           <div className="col-md-3">
-            <img src= {`https://avatars.dicebear.com/v2/avataaars/${username}.svg?options[mood][]=happy`} className="img-fluid rounded-start" alt= {username} style={{height: '200px', width: '200px'}}></img>
+            <img
+              src={`https://avatars.dicebear.com/v2/avataaars/${username}.svg?options[mood][]=happy`}
+              className="img-fluid rounded-start"
+              alt={username}
+              style={{ height: "200px", width: "200px" }}
+            ></img>
           </div>
-          <div className="col-md-9">
-           {this.renderBody()}
-          </div>
+          <div className="col-md-8">{this.renderBody()}</div>
         </div>
-      </div>
+      </Card>
     );
   }
 }
