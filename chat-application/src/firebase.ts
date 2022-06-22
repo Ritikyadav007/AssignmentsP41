f96a69a60a7f18775e998f0679e4ee2446e75e9b
Ritikyadav007 <ritiky986@gmail.com>
2022-06-22 15:10:27 +0530

    setup firebase for this application and configured firebase file

---
+++ chat-application/src/firebase.ts
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyAih8oXNGUo1VpOLXrP9w5y6AIT-JsjIBI',
+  authDomain: 'chat-app-e3bb9.firebaseapp.com',
+  projectId: 'chat-app-e3bb9',
+  storageBucket: 'chat-app-e3bb9.appspot.com',
+  messagingSenderId: '123130287849',
+  appId: '1:123130287849:web:591d4f05b73cb33604c278',
+};
