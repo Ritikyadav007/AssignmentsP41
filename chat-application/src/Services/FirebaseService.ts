73c38cc6f7765018c136222f1722e01daa878d75
Ritikyadav007 <ritiky986@gmail.com>
2022-06-29 09:44:47 +0530

    refactored firebase service and string store error

---
+++ chat-application/src/Services/FirebaseService.ts
@@ -0,0 +1,31 @@
+import firebase from 'firebase/compat/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAih8oXNGUo1VpOLXrP9w5y6AIT-JsjIBI',
+  authDomain: 'chat-app-e3bb9.firebaseapp.com',
+  projectId: 'chat-app-e3bb9',
+  storageBucket: 'chat-app-e3bb9.appspot.com',
+  messagingSenderId: '123130287849',
+  appId: '1:123130287849:web:591d4f05b73cb33604c278',
+};
+
+class FirebaseService {
+  private instance: any;
+
+  constructor(config: any) {
+    if (!this.instance) {
+      this.instance = firebase.initializeApp(config);
+    }
+  }
+
+  get() {
+    return this.instance;
+  }
+}
+
+const firebaseApp = new FirebaseService(firebaseConfig);
+
+export default firebaseApp;
+// const app = firebase.initializeApp(firebaseConfig);
+
+// export default app;
