c2ad7d323cd89763afab38f8583c58bbed398863
Ritikyadav007 <ritiky986@gmail.com>
2022-07-08 15:02:15 +0530

    added basic ui for Chat component

--- chat-application/src/Components/Chats/Chats.tsx
+++ chat-application/src/Components/Chats/Chats.tsx
@@ -1,24 +1,69 @@
 import { Avatar } from 'antd';
 import './Chats.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import CallIcon from '@mui/icons-material/Call';
+import SearchIcon from '@mui/icons-material/Search';
+import SendIcon from '@mui/icons-material/Send';
+import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
+import VideoCallIcon from '@mui/icons-material/VideoCall';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import { getDownloadURL, ref } from 'firebase/storage';
+import { IconButton } from '@mui/material/';
 import { useAuth } from '../../store/AuthContext';
 import storage from '../../Services/StorageService';
 
 export default function Chats() {
   const [userImage, setuserImage] = useState('');
   const { user } = useAuth();
-  const imageRef = ref(storage, `assets/${user.uid}/profileimage.jpg`);
-  getDownloadURL(imageRef).then((url) => {
-    setuserImage(url);
-  });
+
+  useEffect(() => {
+    const imageRef = ref(storage, `assets/${user.uid}/profileimage.jpg`);
+    getDownloadURL(imageRef).then((url) => {
+      setuserImage(url);
+    });
+  }, []);
 
   return (
     <div className="chats">
-      <div className="chats_Items">
+      {/* <div className="chats_Items">
         <Avatar size={80} src={userImage} />
         <h5>Welcome</h5>
         <p>Please select a chat to start messaging</p>
+      </div> */}
+      <div className="chat_header">
+        <Avatar size={45} />
+        <div className="chat_headerInfo">
+          <h3>Chat</h3>
+        </div>
+
+        <div className="chat_headerRight">
+          <IconButton>
+            <SearchIcon />
+          </IconButton>
+          <IconButton>
+            <CallIcon />
+          </IconButton>
+          <IconButton>
+            <VideoCallIcon />
+          </IconButton>
+          <IconButton>
+            <MoreHorizIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className="chat_body">hello</div>
+      <div className="chat_footer">
+        <div className="chat_footerInput">
+          <input type="text" placeholder="Type a Message" />
+          <IconButton>
+            <InsertEmoticonIcon />
+          </IconButton>
+        </div>
+        <div className="chat_footerButton">
+          <button type="submit">
+            <SendIcon />
+          </button>
+        </div>
       </div>
     </div>
   );
