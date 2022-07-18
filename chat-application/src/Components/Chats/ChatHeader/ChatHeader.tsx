7de969f695559767eaa8565d87f927e6fa728d09
Ritikyadav007 <ritiky986@gmail.com>
2022-07-18 14:37:42 +0530

    Created chat header component

---
+++ chat-application/src/Components/Chats/ChatHeader/ChatHeader.tsx
@@ -0,0 +1,47 @@
+/* eslint-disable react/function-component-definition */
+import React, { memo } from 'react';
+import { Avatar } from 'antd';
+import CallIcon from '@mui/icons-material/Call';
+import SearchIcon from '@mui/icons-material/Search';
+import VideoCallIcon from '@mui/icons-material/VideoCall';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import { IconButton } from '@mui/material/';
+import './ChatHeader.css';
+
+type ChatHeaderProps = {
+  chatName: string;
+};
+
+const ChatHeaderButtons = memo(() => {
+  return (
+    <div className="chat_headerRight">
+      <IconButton>
+        <SearchIcon />
+      </IconButton>
+      <IconButton>
+        <CallIcon />
+      </IconButton>
+      <IconButton>
+        <VideoCallIcon />
+      </IconButton>
+      <IconButton>
+        <MoreHorizIcon />
+      </IconButton>
+    </div>
+  );
+});
+
+const ChatHeader: React.FC<ChatHeaderProps> = (props) => {
+  const { chatName } = props;
+  return (
+    <div className="chat_header">
+      <Avatar size={45} />
+      <div className="chat_headerInfo">
+        <h3>{chatName}</h3>
+      </div>
+      <ChatHeaderButtons />
+    </div>
+  );
+};
+
+export default memo(ChatHeader);
