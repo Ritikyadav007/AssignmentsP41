80f68ae53af475fd124698af5612e3570a94d010
Ritikyadav007 <ritiky986@gmail.com>
2022-07-21 15:24:43 +0530

    added last message functionality

--- chat-application/src/Components/Friend/Friend.tsx
+++ chat-application/src/Components/Friend/Friend.tsx
@@ -1,6 +1,7 @@
 import { Avatar } from 'antd';
 import { UserOutlined } from '@ant-design/icons';
 import './Friend.css';
+import { useEffect, useState } from 'react';
 
 type FriendsProps = {
   groupData: any;
@@ -10,6 +11,19 @@ type FriendsProps = {
 export default function Friend(props: FriendsProps) {
   const { groupData, handleClick } = props;
   const { name } = groupData;
+  const [lastMessage, setLastMessage] = useState<string>();
+
+  useEffect(() => {
+    if (groupData.messages === undefined) {
+      setLastMessage('...');
+    } else {
+      const { messages } = groupData;
+      const msgArray = Object.values(messages);
+      const lastMsg: any = msgArray[msgArray.length - 1];
+      setLastMessage(lastMsg.message);
+    }
+  }, []);
+
   return (
     <div className="friend">
       <Avatar size={40} src={<UserOutlined />} />
@@ -20,7 +34,7 @@ export default function Friend(props: FriendsProps) {
         }}
       >
         <h2>{name}</h2>
-        <p>Last Message...</p>
+        <p>{lastMessage}</p>
       </div>
     </div>
   );
